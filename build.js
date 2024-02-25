86448f329b6f69a8c9ab64fbeef3de68b54621b6
Dr. Vortex <jp@drvortex.dev>
2024-02-25 10:10:49 -0600

    Initial

---
+++ build.js
@@ -0,0 +1,52 @@
+/* eslint-env node */
+import { context } from 'esbuild';
+import { execSync } from 'node:child_process';
+import { rmSync } from 'node:fs';
+import path from 'node:path';
+import { parseArgs } from 'node:util';
+
+const { values: options } = parseArgs({
+	options: {
+		watch: { short: 'w', type: 'boolean', default: false },
+		preserve: { short: 'p', type: 'boolean', default: false },
+		out: { short: 'o', type: 'string', default: 'dist' },
+	},
+});
+
+const ctx = await context({
+	entryPoints: ['src/index.ts'],
+	outfile: path.join(options.out, 'min.js'),
+	format: 'esm',
+	platform: 'neutral',
+	keepNames: true,
+	sourcemap: true,
+	bundle: true,
+	minify: true,
+	plugins: [
+		{
+			name: 'types',
+			setup(build) {
+				build.onStart(() => {
+					if (!options.preserve) {
+						rmSync(options.out, { force: true, recursive: true });
+					}
+				});
+				build.onEnd(() => {
+					try {
+						execSync('npx tsc -p tsconfig.json --outDir ', { stdio: 'inherit' });
+					} catch (error) {
+						console.error(error.stack);
+					}
+				});
+			},
+		},
+	],
+});
+
+if (options.watch) {
+	console.log('Watching...');
+	await ctx.watch();
+} else {
+	await ctx.rebuild();
+	await ctx.dispose();
+}
